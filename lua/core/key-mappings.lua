3f0fa92de491b4b7e27c42241ee6ee4d02372096
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-09-04 19:14:20 +0100

    Edit key mappings

--- lua/core/key-mappings.lua
+++ lua/core/key-mappings.lua
@@ -1,4 +1,15 @@
-vim.g.mapleader = ';' -- Change the leader key from '\' to ';' (',' is also popular)
+local map = vim.keymap.set
 
 -- http://vim.wikia.com/wiki/Avoid_the_escape_key
+
+vim.g.mapleader = ' ' -- Change the leader key from '\' to space (',' and ';' are also popular)
+
+map('n', ';', ':') -- Enter cmdline mode quicker
+map('n', ':', ';') -- Note: Use Hop or text object for repeats of f/F/t/T
+
+-- window-moving
+map('n', '<C-h>', '<C-w>h')
+map('n', '<C-j>', '<C-w>j')
+map('n', '<C-k>', '<C-w>k')
+map('n', '<C-l>', '<C-w>l')
 
--- lua/plugins/telescope/key-mappings.lua
+++ lua/plugins/telescope/key-mappings.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set -- Shorten function name
 
 map('n', '<leader>f', function() require('telescope.builtin').builtin() end)
-map('n', '<leader>en', function() require('plugins.telescope').edit_nvim() end)
+map('n', '<leader>en', function() require('plugins.telescope').edit_nvim() end, {desc = 'Edit nvim config'})
+
