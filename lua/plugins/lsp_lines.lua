26d4d608d89fab650d0bc4f895d5dbea1bfcbf78
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-09-04 19:10:37 +0100

    Distraction

--- lua/plugins/lsp_lines.lua
+++ lua/plugins/lsp_lines.lua
@@ -1,6 +1,6 @@
--- Disable virtual_text since it's redundant due to lsp_lines.
 vim.diagnostic.config {
-  virtual_text = false,
+  virtual_text = false, -- Disable virtual_text since it's redundant due to lsp_lines.
+  virtual_lines = false
 }
 
 vim.keymap.set(
