afd4c65c27ca63c4e740a7823fea1f3e956d54d5
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-12-01 17:01:08 +0000

    Use of debugpy in virtualenv

--- lua/plugins/dap/python.lua
+++ lua/plugins/dap/python.lua
@@ -1,7 +1,13 @@
 local dap = require('dap')
 dap.adapters.python = {
   type = 'executable';
-  command = vim.fn.getcwd() .. '/.venv/bin/python';
+  command = function()
+    if os.getenv('VIRTUAL_ENV') ~= nil then
+      return os.getenv('VIRTUAL_ENV') .. '/bin/python'
+    else
+      return nil
+    end
+  end;
   args = { '-m', 'debugpy.adapter' };
 }
 
