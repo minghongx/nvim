aff55a51a1087954e4c811b5d296f727434c347f
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2023-05-14 17:21:48 +0100

    See https://github.com/ellisonleao/gruvbox.nvim/issues/220

--- lua/plugins/themes/gruvbox.lua
+++ lua/plugins/themes/gruvbox.lua
@@ -2,7 +2,12 @@ require('gruvbox').setup {
   undercurl = true,
   underline = true,
   bold = true,
-  italic = false,
+  italic = {
+    strings = false,
+    comments = false,
+    operators = false,
+    folds = true,
+  },
   strikethrough = true,
   invert_selection = false,
   invert_signs = false,
