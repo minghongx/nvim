8fd1ca466f66c1797af723e9f5237b5b32de3b5b
Minghong Xu <86758413+MinghonZi@users.noreply.github.com>
2023-10-28 15:49:34 +0100

    Additional specs for Mason

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,9 @@
 return {
   {
     'williamboman/mason.nvim',
+    cmd = 'Mason',
+    keys = { { '<leader>cm', '<cmd>Mason<cr>', desc = 'Mason' } },
+    build = ':MasonUpdate',
     opts = {
       ui = {
         icons = {
