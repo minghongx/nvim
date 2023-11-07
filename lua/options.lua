b55770c47008f58a9bb3d12a1923acc17172ebf0
Minghong Xu <86758413+MinghonZi@users.noreply.github.com>
2023-11-07 02:04:32 +0000

    Add sensible options

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,25 @@
+-- Sync clipboard between OS and Neovim
+vim.o.clipboard = 'unnamedplus'
+
+-- Save undo history
+vim.o.undofile = true
+
+vim.o.updatetime = 250
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
 -- Show relative line numbers and the actual number of the line with the cursor
-vim.wo.rnu = true; vim.wo.nu = true
+vim.wo.relativenumber = true; vim.wo.number = true
 
 vim.o.list = true; vim.o.lcs = 'eol:¬,trail:·,tab:»·,precedes:❮,extends:❯'
 
+-- Enable break indent
 vim.o.breakindent = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = 'yes'
+
+-- NOTE: Make sure the terminal supports this
+vim.o.termguicolors = true
